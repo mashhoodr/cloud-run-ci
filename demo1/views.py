f3e7b96032015fbb891462d3515fd12c44d7e716
Mashhood Rastgar <mashhoodr@gmail.com>
2019-09-28 09:33:19 +0500

    updated view

--- demo1/views.py
+++ demo1/views.py
@@ -4,4 +4,4 @@ from rest_framework.response import Response
 
 class HelloWorld(APIView):
     def get(self, request):
-        return Response("HELLO WORLD! from Django.")
+        return Response("HELLO WORLD!")
